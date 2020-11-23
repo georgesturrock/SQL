eaa5e17b1ad78eefa17cdfe894e112dc1a1db635
georgesturrock <george.sturrock@gmail.com>
2020-11-23 14:50:48 -0600

    Add files via upload

---
+++ createARMTestLogins.sql
@@ -0,0 +1,22 @@
+-- ======================================================================================
+-- Create SQL Login template for Azure SQL Database and Azure SQL Data Warehouse Database
+-- ======================================================================================
+
+-- Add to master first!
+CREATE LOGIN [armwrite] 
+	WITH PASSWORD = 'wr!t30nly' 
+GO
+
+-- ========================================================================================
+-- Create User as DBO template for Azure SQL Database and Azure SQL Data Warehouse Database
+-- ========================================================================================
+-- For login <login_name, sysname, login_name>, create a user in the database
+-- Add user in Target database after creating login above
+CREATE USER [armwrite]
+	FOR LOGIN [armwrite]
+	WITH DEFAULT_SCHEMA = [dbo]
+GO
+
+-- Add user to the database owner role
+EXEC sp_addrolemember 'db_datawriter', 'armwrite' 
+GO
